9ffdbc4dcc1fe1ff00df7594c3f843f627b65723
Srinivas Karnati <srinivas@Srinivass-MacBook-Air.local>
2026-04-14 12:23:12 +0530

    change identitystore custom modules to support the null values for variables

--- aws/aws_identitystore_group/aws_identitystore_group.tf
+++ aws/aws_identitystore_group/aws_identitystore_group.tf
@@ -1,6 +1,5 @@
 resource "aws_identitystore_group" "this" {
-  description       = var.description
+  description       = var.description != null && var.description != "" ? var.description : null
   display_name      = var.display_name
   identity_store_id = var.identity_store_id
-
 }
--- aws/aws_identitystore_user/aws_identitystore_user.tf
+++ aws/aws_identitystore_user/aws_identitystore_user.tf
@@ -1,26 +1,27 @@
 resource "aws_identitystore_user" "this" {
-  display_name       = var.display_name
-  identity_store_id  = var.identity_store_id
-  locale             = var.locale
-  nickname           = var.nickname
-  preferred_language = var.preferred_language
-  profile_url        = var.profile_url
-  timezone           = var.timezone
-  title              = var.title
-  user_name          = var.user_name
-  user_type          = var.user_type
+  display_name      = var.display_name
+  identity_store_id = var.identity_store_id
+  user_name         = var.user_name
+
+  locale             = var.locale != null && var.locale != "" ? var.locale : null
+  nickname           = var.nickname != null && var.nickname != "" ? var.nickname : null
+  preferred_language = var.preferred_language != null && var.preferred_language != "" ? var.preferred_language : null
+  profile_url        = var.profile_url != null && var.profile_url != "" ? var.profile_url : null
+  timezone           = var.timezone != null && var.timezone != "" ? var.timezone : null
+  title              = var.title != null && var.title != "" ? var.title : null
+  user_type          = var.user_type != null && var.user_type != "" ? var.user_type : null
 
   dynamic "addresses" {
     for_each = var.addresses != null ? var.addresses : []
     content {
-      country        = addresses.value.country
-      formatted      = addresses.value.formatted
-      locality       = addresses.value.locality
-      postal_code    = addresses.value.postal_code
+      country        = addresses.value.country != null && addresses.value.country != "" ? addresses.value.country : null
+      formatted      = addresses.value.formatted != null && addresses.value.formatted != "" ? addresses.value.formatted : null
+      locality       = addresses.value.locality != null && addresses.value.locality != "" ? addresses.value.locality : null
+      postal_code    = addresses.value.postal_code != null && addresses.value.postal_code != "" ? addresses.value.postal_code : null
       primary        = addresses.value.primary
-      region         = addresses.value.region
-      street_address = addresses.value.street_address
-      type           = addresses.value.type
+      region         = addresses.value.region != null && addresses.value.region != "" ? addresses.value.region : null
+      street_address = addresses.value.street_address != null && addresses.value.street_address != "" ? addresses.value.street_address : null
+      type           = addresses.value.type != null && addresses.value.type != "" ? addresses.value.type : null
     }
   }
 
@@ -28,20 +29,21 @@ resource "aws_identitystore_user" "this" {
     for_each = var.emails != null ? var.emails : []
     content {
       primary = emails.value.primary
-      type    = emails.value.type
-      value   = emails.value.value
+      type    = emails.value.type != null && emails.value.type != "" ? emails.value.type : null
+      value   = emails.value.value != null && emails.value.value != "" ? emails.value.value : null
     }
   }
 
   dynamic "name" {
-    for_each = var.name
+    for_each = var.name != null ? var.name : []
     content {
-      family_name      = name.value.family_name
-      formatted        = name.value.formatted
-      given_name       = name.value.given_name
-      honorific_prefix = name.value.honorific_prefix
-      honorific_suffix = name.value.honorific_suffix
-      middle_name      = name.value.middle_name
+      family_name = name.value.family_name
+      given_name  = name.value.given_name
+
+      formatted        = name.value.formatted != null && name.value.formatted != "" ? name.value.formatted : null
+      honorific_prefix = name.value.honorific_prefix != null && name.value.honorific_prefix != "" ? name.value.honorific_prefix : null
+      honorific_suffix = name.value.honorific_suffix != null && name.value.honorific_suffix != "" ? name.value.honorific_suffix : null
+      middle_name      = name.value.middle_name != null && name.value.middle_name != "" ? name.value.middle_name : null
     }
   }
 
@@ -49,9 +51,8 @@ resource "aws_identitystore_user" "this" {
     for_each = var.phone_numbers != null ? var.phone_numbers : []
     content {
       primary = phone_numbers.value.primary
-      type    = phone_numbers.value.type
-      value   = phone_numbers.value.value
+      type    = phone_numbers.value.type != null && phone_numbers.value.type != "" ? phone_numbers.value.type : null
+      value   = phone_numbers.value.value != null && phone_numbers.value.value != "" ? phone_numbers.value.value : null
     }
   }
-
 }
